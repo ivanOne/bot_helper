4ad6313de4d396d4183b94a8f43fa57f147763a1
IvanOne <turnthepage41@gmail.com>
2016-08-03 07:19:09 +0300

    default url

--- runserver.py
+++ runserver.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, Response
 from sentry.controller import sentry
 
 app = Flask(__name__)
 app.register_blueprint(sentry)
+
+
+@app.route("/")
+def default_response():
+    return Response("OK")
 
 
 if __name__ == '__main__':
