5eb2a000b93b70f381a7e9d9e6d5fc811b39431c
IvanOne <turnthepage41@gmail.com>
2016-08-05 07:24:03 +0300

    add logg

--- runserver.py
+++ runserver.py
@@ -1,8 +1,27 @@
-from flask import Flask, Response
+import datetime
+
+from flask import Flask, Response, request, logging
 from sentry.controller import sentry
 
 app = Flask(__name__)
 app.register_blueprint(sentry)
+
+
+@app.before_request
+def pre_request_logging():
+    if 'text/html' in request.headers['Accept']:
+        dt = datetime.datetime.today().ctime()
+        addr = request.remote_addr
+        method = request.method
+        url = request.url
+        data = request.data
+        headers = ', \n'.join([': '.join(x) for x in request.headers])
+        kw = {'dt': dt, 'addr': addr, 'method': method, 'url': url, 'data': data, 'headers': headers}
+        str_log = "{dt} \n {addr} \n {method} \n {url} \n {data} \n headers: \n {headers}\n -------------------\n"\
+            .format(**kw)
+        f = open('log.txt', 'w')
+        f.write(str_log)
+        f.close()
 
 
 @app.route("/")
